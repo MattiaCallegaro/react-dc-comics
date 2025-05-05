810672c3e1afe4e4f8dcb0409ff87c3dd85fc0e8
MattiaCallegaro <mattiacallegarocioi@gmail.com>
2025-05-05 17:24:49 +0200

    [UPD] Add footer list

--- src/assets/img/components/Footer.jsx
+++ src/assets/img/components/Footer.jsx
@@ -1,6 +1,47 @@
 
 const Footer = () => {
-    return <footer></footer>
+    return <footer className="container">
+        <div className="footer-top">
+            <div className="footer-col">
+                <div>
+                    <h4>DC COMICS</h4>
+                    <ul>
+                        <li><a href="#" className="active">Characters</a></li>
+                        <li><a href="#" className="active">Comics</a></li>
+                        <li><a href="#" className="active">Movie</a></li>
+                        <li><a href="#" className="active">Tv</a></li>
+                        <li><a href="#" className="active">Games</a></li>
+                        <li><a href="#" className="active">Videos</a></li>
+                        <li><a href="#" className="active">News</a></li>
+                    </ul>
+                    <h4>SHOP</h4>
+                    <ul>
+                        <li><a href="#" className="active">Shop DC</a></li>
+                        <li><a href="#" className="active">Shop DC Collectibles</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div className="footer-col">
+                <div>
+                    <h4>DC</h4>
+                    <ul>
+                        <li><a href="#" className="active">Terms Of Use</a></li>
+                        <li><a href="#" className="active">Privacy policy (New)</a></li>
+                        <li><a href="#" className="active">Ad Choices</a></li>
+                        <li><a href="#" className="active">Advesrtising</a></li>
+                        <li><a href="#" className="active">Jobs</a></li>
+                        <li><a href="#" className="active">Subscriptions</a></li>
+                        <li><a href="#" className="active">Talent Workshop</a></li>
+                        <li><a href="#" className="active">CPSC Certificates</a></li>
+                        <li><a href="#" className="active">Ratings</a></li>
+                        <li><a href="#" className="active">Shop Help</a></li>
+                        <li><a href="#" className="active">Contact Us</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div className="footer-col"></div>
+        </div>
+    </footer>
 }
 
 export default Footer
