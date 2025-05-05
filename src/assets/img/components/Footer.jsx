9abd5326e44357745a7ad74e19501e6b7c4a418a
MattiaCallegaro <mattiacallegarocioi@gmail.com>
2025-05-05 18:05:36 +0200

    [UPD] Add social logo

--- src/assets/img/components/Footer.jsx
+++ src/assets/img/components/Footer.jsx
@@ -1,60 +1,76 @@
 
 const Footer = () => {
     return <footer>
-        <div className="footer-top container">
-            <div className="footer-col">
-                <div>
-                    <h4>DC COMICS</h4>
-                    <ul>
-                        <li><a href="#" className="active">Characters</a></li>
-                        <li><a href="#" className="active">Comics</a></li>
-                        <li><a href="#" className="active">Movie</a></li>
-                        <li><a href="#" className="active">Tv</a></li>
-                        <li><a href="#" className="active">Games</a></li>
-                        <li><a href="#" className="active">Videos</a></li>
-                        <li><a href="#" className="active">News</a></li>
-                    </ul>
-                    <h4>SHOP</h4>
-                    <ul>
-                        <li><a href="#" className="active">Shop DC</a></li>
-                        <li><a href="#" className="active">Shop DC Collectibles</a></li>
-                    </ul>
+        <div className="container-footer">
+            <div className="footer-top">
+                <div className="footer-col">
+                    <div>
+                        <h4>DC COMICS</h4>
+                        <ul>
+                            <li><a href="#" className="active">Characters</a></li>
+                            <li><a href="#" className="active">Comics</a></li>
+                            <li><a href="#" className="active">Movie</a></li>
+                            <li><a href="#" className="active">Tv</a></li>
+                            <li><a href="#" className="active">Games</a></li>
+                            <li><a href="#" className="active">Videos</a></li>
+                            <li><a href="#" className="active">News</a></li>
+                        </ul>
+                        <h4>SHOP</h4>
+                        <ul>
+                            <li><a href="#" className="active">Shop DC</a></li>
+                            <li><a href="#" className="active">Shop DC Collectibles</a></li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
-            <div className="footer-col">
-                <div>
-                    <h4>DC</h4>
-                    <ul>
-                        <li><a href="#" className="active">Terms Of Use</a></li>
-                        <li><a href="#" className="active">Privacy policy (New)</a></li>
-                        <li><a href="#" className="active">Ad Choices</a></li>
-                        <li><a href="#" className="active">Advesrtising</a></li>
-                        <li><a href="#" className="active">Jobs</a></li>
-                        <li><a href="#" className="active">Subscriptions</a></li>
-                        <li><a href="#" className="active">Talent Workshop</a></li>
-                        <li><a href="#" className="active">CPSC Certificates</a></li>
-                        <li><a href="#" className="active">Ratings</a></li>
-                        <li><a href="#" className="active">Shop Help</a></li>
-                        <li><a href="#" className="active">Contact Us</a></li>
-                    </ul>
+                <div className="footer-col">
+                    <div>
+                        <h4>DC</h4>
+                        <ul>
+                            <li><a href="#" className="active">Terms Of Use</a></li>
+                            <li><a href="#" className="active">Privacy policy (New)</a></li>
+                            <li><a href="#" className="active">Ad Choices</a></li>
+                            <li><a href="#" className="active">Advesrtising</a></li>
+                            <li><a href="#" className="active">Jobs</a></li>
+                            <li><a href="#" className="active">Subscriptions</a></li>
+                            <li><a href="#" className="active">Talent Workshop</a></li>
+                            <li><a href="#" className="active">CPSC Certificates</a></li>
+                            <li><a href="#" className="active">Ratings</a></li>
+                            <li><a href="#" className="active">Shop Help</a></li>
+                            <li><a href="#" className="active">Contact Us</a></li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
-            <div className="footer-col">
-                <div>
-                    <h4>SITES</h4>
-                    <ul>
-                        <li><a href="#" className="active">DC</a></li>
-                        <li><a href="#" className="active">MAD Magazine</a></li>
-                        <li><a href="#" className="active">DC Kids</a></li>
-                        <li><a href="#" className="active">DC Universe</a></li>
-                        <li><a href="#" className="active">DC Power Visa</a></li>
+                <div className="footer-col">
+                    <div>
+                        <h4>SITES</h4>
+                        <ul>
+                            <li><a href="#" className="active">DC</a></li>
+                            <li><a href="#" className="active">MAD Magazine</a></li>
+                            <li><a href="#" className="active">DC Kids</a></li>
+                            <li><a href="#" className="active">DC Universe</a></li>
+                            <li><a href="#" className="active">DC Power Visa</a></li>
 
-                    </ul>
+                        </ul>
+                    </div>
                 </div>
+                <div className="footer-col"></div>
             </div>
-            <div className="footer-col"></div>
         </div>
-    </footer>
+        <div className="footer-bottom">
+            <div>
+                <button className="btn">SIGN-UP NOW!</button>
+            </div>
+            <div className="social">
+                <a href="#" className="active"><h4>FOLLOW US</h4></a>
+                <a href="#" className="active"><img src="footer-facebook.png" alt="facebook" /></a>
+                <a href="#" className="active"><img src="footer-twitter.png" alt="facebook" /></a>
+                <a href="#" className="active"><img src="footer-youtube.png" alt="facebook" /></a>
+                <a href="#" className="active"><img src="footer-pinterest.png" alt="facebook" /></a>
+                <a href="#" className="active"><img src="footer-periscope.png" alt="facebook" /></a>
+            </div>
+
+        </div>
+    </footer >
 }
 
 export default Footer
