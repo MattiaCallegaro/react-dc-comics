65dd8777aa4be7148afafca184881590f4cea71c
MattiaCallegaro <mattiacallegarocioi@gmail.com>
2025-05-05 15:03:54 +0200

    [UPD] Add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-
-import './App.css'
+// importo i miei components
+import Header from "./assets/img/components/Header"
+import Main from "./assets/img/components/Main"
+import Footer from "./assets/img/components/Footer"
 
 function App() {
-  return <h1>Ciao</h1>
-
+  return
+  <>
+    <Header />
+    <Main />
+    <Footer />
+  </>
 }
 
 export default App
