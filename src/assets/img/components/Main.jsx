7c29e9c1246ea646832f9f1ca7e38010adb40d0e
MattiaCallegaro <mattiacallegarocioi@gmail.com>
2025-05-05 16:55:15 +0200

    [UPD] Main completed

--- src/assets/img/components/Main.jsx
+++ src/assets/img/components/Main.jsx
@@ -4,6 +4,30 @@ const Main = () => {
         <section>
             <h3>--Content goes here--</h3>
         </section>
+        <menu>
+            <ul>
+                <li>
+                    <img src="buy-comics-digital-comics.png" alt="Digital" />
+                    <a href="#" className="active"><span>DIGITAL COMICS</span></a>
+                </li>
+                <li>
+                    <img src="buy-comics-merchandise.png" alt="Digital" />
+                    <a href="#" className="active"><span>DC MERCHANDISE</span></a>
+                </li>
+                <li>
+                    <img src="buy-comics-subscriptions.png" alt="Digital" />
+                    <a href="#" className="active"><span>SUBSCRIPTION</span></a>
+                </li>
+                <li>
+                    <img src="buy-comics-shop-locator.png" alt="Digital" />
+                    <a href="#" className="active"><span>COMIC SHOP LOCATOR</span></a>
+                </li>
+                <li>
+                    <img className="visa" src="buy-dc-power-visa.svg" alt="Digital" />
+                    <a href="#" className="active"><span>DC POWER VISA</span></a>
+                </li>
+            </ul>
+        </menu>
     </main>
 }
 
